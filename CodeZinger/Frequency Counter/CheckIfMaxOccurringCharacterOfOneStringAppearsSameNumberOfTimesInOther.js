746a2b3efaf5b883418c330538cc8a7d0a47c171
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-03-10 21:54:03 +0530

    changes done

--- CodeZinger/Frequency Counter/CheckIfMaxOccurringCharacterOfOneStringAppearsSameNumberOfTimesInOther.js
+++ CodeZinger/Frequency Counter/CheckIfMaxOccurringCharacterOfOneStringAppearsSameNumberOfTimesInOther.js
@@ -30,24 +30,58 @@ Explanation:
 */
 
 function solution(str1, str2) {
-    let checkAna1 = {};
-    let checkAna2 = {};
+    // let checkAna1 = {};
+    // let checkAna2 = {};
+
+    // for (let i = 0; i < str1.length; i++) {
+    //     checkAna1[str1[i]] = (checkAna1[str1[i]] || 0) + 1;
+    // }
+
+    // for (let i = 0; i < str2.length; i++) {
+    //     checkAna2[str2[i]] = (checkAna2[str2[i]] || 0) + 1;
+    // }
+
+    // for (let freq in checkAna1) {
+    //     if (checkAna1[freq] == checkAna2[freq]) {
+    //         console.log(freq);
+    //         return "Yes";
+    //     }
+    // }
+    // return "No";
+
+    //=========or========
+    let map = new Map()
 
     for (let i = 0; i < str1.length; i++) {
-        checkAna1[str1[i]] = (checkAna1[str1[i]] || 0) + 1;
+        map.set(str1[i], (map.get(str1[i]) || 0) + 1)
     }
+    let maximumFrequency = 0;
+    let frequentElement = "";
+
+    map.forEach((value, key) => {
+        if (value > maximumFrequency) {
+            maximumFrequency = value
+            frequentElement = key
+        }
+    })
+    map.clear();
 
     for (let i = 0; i < str2.length; i++) {
-        checkAna2[str2[i]] = (checkAna2[str2[i]] || 0) + 1;
+        map.set(str2[i], (map.get(str2[i]) || 0) + 1);
     }
 
-    for (let freq in checkAna1) {
-        if (checkAna1[freq] == checkAna2[freq]) {
+    console.log(map);
+    map.forEach((value, key) => {
+        if (maximumFrequency == value && frequentElement == key) {
+            console.log(map);
+            console.log(maximumFrequency,frequentElement);
+            console.log(value,key);
+            
             return "Yes";
         }
-    }
+    });
     return "No";
 }
 
-let a = solution("hello world", "albcllsyaaaaaaaa");
+let a = solution("hezzllo world", "albcllzzsyaaaaaaaa");
 console.log(a);
