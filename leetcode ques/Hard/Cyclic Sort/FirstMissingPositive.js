537aab967a6255f622126e3457ae9d0eb002c3e7
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-03-16 00:00:26 +0530

    First Missing Positive done

---
+++ leetcode ques/Hard/Cyclic Sort/FirstMissingPositive.js
@@ -0,0 +1,54 @@
+/*
+Given an unsorted integer array nums, return the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+Example 3:
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+Explanation: The smallest positive integer 1 is missing.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+*/
+
+var firstMissingPositive = function (nums) {
+    let i = 0;
+    while (i < nums.length) {
+        let currectIndex = nums[i] - 1;
+
+        if ((nums[i] > 0 && nums[i] <= nums.length) && nums[i] != nums[currectIndex]) {
+            let temp = nums[i];
+            nums[i] = nums[currectIndex];
+            nums[currectIndex] = temp;
+        } else {
+            i++;
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (i != nums[i] - 1) {
+            return i + 1;
+        }
+    }
+    return nums.length + 1;
+}
+
+// let a =firstMissingPositive([3,4,-1,1]);
+let a = firstMissingPositive([1]);
+console.log(a);
