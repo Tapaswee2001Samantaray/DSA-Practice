e059712f0f19fe5ebffd6cd02bc1a69a2923c9d8
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-03-19 12:48:23 +0530

    Height Checker done

---
+++ leetcode ques/Easy/Counting Sort/HeightChecker.js
@@ -0,0 +1,71 @@
+/*
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in 
+non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected 
+height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the 
+height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+ 
+
+Example 1:
+
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+Example 2:
+
+Input: heights = [5,1,2,3,4]
+Output: 5
+Explanation:
+heights:  [5,1,2,3,4]
+expected: [1,2,3,4,5]
+All indices do not match.
+Example 3:
+
+Input: heights = [1,2,3,4,5]
+Output: 0
+Explanation:
+heights:  [1,2,3,4,5]
+expected: [1,2,3,4,5]
+All indices match.
+ 
+
+Constraints:
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+*/
+
+var heightChecker = function (heights) {
+    let hashArr = new Array(101).fill(0);
+
+    for (let i = 0; i < heights.length; i++) {
+        hashArr[heights[i]]++;
+    }
+
+    let hashArrIndex = 0;
+    let result = 0;
+
+    for (let i = 0; i < heights.length; i++) {
+        while (hashArr[hashArrIndex] == 0) {
+            hashArrIndex++;
+        }
+
+        if (heights[i] != hashArrIndex) {
+            result++;
+        }
+
+        hashArr[hashArrIndex]--;
+    }
+    return result;
+}
+
+// let a = heightChecker([1, 1, 4, 2, 1, 3]);
+let a = heightChecker([2, 6, 8, 6, 5, 2, 4, 3, 7, 3, 7, 5, 6, 6, 2, 4, 4, 6, 8, 4, 5]);
+console.log(a);
