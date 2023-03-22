5ecfdf862dcb41a33d9c768f956031d40a7f4ffb
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-03-22 11:44:19 +0530

    Merge Two Sorted Lists done

---
+++ leetcode ques/Easy/Linked List/MergeTwoSortedLists.js
@@ -0,0 +1,99 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
+
+class ListNode {
+    constructor(value, next) {
+        this.value = value == undefined ? 0 : value;
+        this.next = next == undefined ? null : next;
+    }
+}
+
+let convertArr1ToLL = function (arr) {
+    let head = new ListNode(arr[0]);
+    let pointer = head;
+
+    for (let i = 1; i < arr.length; i++) {
+        let node = new ListNode(arr[i]);
+        pointer.next = node;
+        pointer = pointer.next;
+    }
+    return head;
+}
+
+let convertArr2ToLL = function (arr) {
+    let head = new ListNode(arr[0]);
+    let pointer = head;
+
+    for (let i = 1; i < arr.length; i++) {
+        let node = new ListNode(arr[i]);
+        pointer.next = node;
+        pointer = pointer.next;
+    }
+    return head;
+}
+
+let list1 = convertArr1ToLL([1, 2, 4]);
+let list2 = convertArr2ToLL([1, 3, 4]);
+// console.log(list1, list2);
+
+//=========sol=======
+var mergeTwoLists = function (list1, list2) {
+    let dummyNode = new ListNode();
+    let pointer = dummyNode;
+
+    while (list1 != null && list2 != null) {
+        if (list1.value < list2.value) {
+            pointer.next = list1;
+            list1 = list1.next;
+        } else {
+            pointer.next = list2;
+            list2 = list2.next;
+        }
+        pointer = pointer.next;
+    }
+
+    while (list1 != null) {
+        pointer.next = list1;
+        list1 = list1.next;
+        pointer = pointer.next;
+    }
+
+    while (list2 != null) {
+        pointer.next = list2;
+        list2 = list2.next;
+        pointer = pointer.next;
+    }
+
+    return dummyNode.next;
+}
+
+let a = mergeTwoLists(list1, list2);
+console.log(a);
