0f00199bdf042246269555532f0b29b241cff25d
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-03-30 15:45:56 +0530

    Diameter of Binary Tree done

---
+++ leetcode ques/Easy/Binary Search Tree/DiameterOfBinaryTree.js
@@ -0,0 +1,48 @@
+/*
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass 
+through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+*/
+
+var diameterOfBinaryTree = function (root) {
+    let result = [];
+
+    function calMaxDepth(root) {
+        if (root == null) {
+            return 0;
+        }
+
+        let leftHeight = calMaxDepth(root.left);
+        let rightHeight = calMaxDepth(root.right);
+
+        let maxDepth = Math.max(leftHeight, rightHeight) + 1;
+
+        result = Math.max(result, leftHeight + rightHeight);
+
+        return maxDepth;
+    }
+    calMaxDepth(root);
+    return result;
+};
