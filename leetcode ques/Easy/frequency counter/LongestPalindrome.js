25bf8ee125fcc8ac8ef14da8bb40c39299cd8818
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-04-03 18:41:58 +0530

    Longest Palindrome done

---
+++ leetcode ques/Easy/frequency counter/LongestPalindrome.js
@@ -0,0 +1,49 @@
+/*
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built 
+with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+ 
+
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+Example 2:
+
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+
+Constraints:
+
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+*/
+
+var longestPalindrome = function (s) {
+    // let freq={};
+    let map = new Map();
+
+    let longestPalindrome = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        // freq[s[i]] = (freq[s[i]] || 0) + 1;
+        map.set(s[i], (map.get(s[i]) || 0) + 1);
+    }
+
+    for (let index of map) {
+        if ((index[1] & 1) && (longestPalindrome & 1)) {
+            longestPalindrome += index[1] - 1;
+        } else {
+            longestPalindrome += index[1];
+        }
+    }
+    return longestPalindrome;
+}
+
+let a = longestPalindrome("abccccdd");
+console.log(a);
